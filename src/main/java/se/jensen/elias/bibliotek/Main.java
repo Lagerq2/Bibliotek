8744e943a804fd8a69896f8cce51c0d19d2809c0
Elias Lagerqvist <eliaslagerqvist@outlook.com>
2025-09-25 14:25:32 +0200

    Fortsättning på menyn och val

--- src/main/java/se/jensen/elias/bibliotek/Main.java
+++ src/main/java/se/jensen/elias/bibliotek/Main.java
@@ -8,18 +8,19 @@ public class Main {
 
     static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Book bok = new Book();
+
         List<Book> bocker = new ArrayList<>();
         while (true) {
             System.out.println("Biblioteket");
             System.out.println("Välj vad du vill göra:");
             System.out.println("[1] Lägg till bok");
             System.out.println("[2] Ta bort bok");
-            System.out.println("[3] Sök bok");
+            System.out.println("[3] Skriv ut böcker");
             System.out.println("[4] Avsluta program");
             String val = scanner.nextLine();
             switch (val) {
                 case "1":
+                    Book bok = new Book();
                     System.out.println("Lägg Till bok:");
                     System.out.println("Namn: ");
                     String name = scanner.nextLine();
@@ -28,14 +29,23 @@ public class Main {
                     bok.setTitle(name);
                     bok.setIsbn(isbn);
                     bocker.add(bok);
-                    System.out.println(bok.toString());
+                    break;
 
                 case "2":
-                    System.out.println("Ta bort bok");
+                    int i = 1;
+                    for (Book b : bocker) {
+                        System.out.println("Tryck [" + i + "] För att ta bort: " + b);
+                        i++;
+                    }
+                    int raderaVal = scanner.nextInt();
+                    raderaVal--;
+                    bocker.remove(raderaVal);
+                    break;
                 case "3":
                     for (Book b : bocker) {
-                        System.out.println(b.toString());
+                        System.out.println(b);
                     }
+                    break;
                 case "4":
                     return;
             }
